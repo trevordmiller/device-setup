ed2459fcc0378cb15b20d16ac9ccf895f8663ea7
Trevor D. Miller <trevordmiller@icloud.com>
2016-08-30 11:26:09 -0600

    Update custom colorscheme draft whites/blacks

--- dotfiles/.hyperterm.js
+++ dotfiles/.hyperterm.js
@@ -1,17 +1,26 @@
-const black = '#2A2E35';
+const dimGrey = '#5C6F68';
+const gunMetal = '#2A2E35';
+const gunMetal2 = '#2D3047';
+const purpleTaupe = '#484349';
+const darkElectricBlue = '#5D737E';
+const stormCloud = '#4F616B';
+const outerSpace = '#404F56';
+const richBlack = '#022F40';
+const onyx = '#36393B';
+
+const black = onyx;
 const red = '#FF7C7A';
 const green = '#8FD5A6';
 const yellow = '#FFE57F';
 const blue = '#378AAD';
 const magenta = '#8C82AF';
 const cyan = '#8CCEC6';
-const white = '#EEEBD0';
-const lightBlack = '#3C424C';
+const white = '#F4F1DE';
+const lightBlack = '#817F823';
 const lightWhite = '#FFFFFA';
 
 module.exports = {
   plugins: [
-    // 'hyperterm-atom-dark',
   ],
   config: {
     padding: '17px',
