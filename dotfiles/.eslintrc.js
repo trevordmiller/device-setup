6f7f1bb5d5a03f4795984dbdd0a3c45f6338a3e1
Trevor D. Miller <trevordmiller@icloud.com>
2016-09-18 00:17:00 -0600

    Add settings while creating Nova repos

---
+++ dotfiles/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "env": {
+    "es6": true,
+    "node": true,
+    "commonjs": true,
+    "browser": true,
+    "jest": true,
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module",
+  },
+  "plugins": ["react"],
+  "rules": {
+    "linebreak-style": ["error", "unix"],
+    "indent": ["error", 2],
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"],
+    "comma-dangle": ["error", "always-multiline"],
+  }
+}
