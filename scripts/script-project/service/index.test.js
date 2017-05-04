9e600fed9b58aab165bbf8aa7886964eec20af5a
Trevor D. Miller <trevordmiller@icloud.com>
2017-05-03 21:31:00 -0600

    Fix service scaffolding

--- scripts/script-project/service/index.test.js
+++ scripts/script-project/service/index.test.js
@@ -1,8 +1,6 @@
-const micro = require('micro')
 const fetch = require('node-fetch')
-const helloWorld = require('.')
 
-test('responds with hello world', () => {
+test('responds with hello world', async () => {
   const response = await fetch('http://localhost:3000')
   const json = await response.json()
   expect(json.greeting).toEqual('hello world')
