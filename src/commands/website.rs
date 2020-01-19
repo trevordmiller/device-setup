59d139a3e9d7ed2148fe79bfa8e08103362266a6
Trevor D. Miller <5497885+trevordmiller@users.noreply.github.com>
2020-01-19 14:37:47 -0700

    Break out markdown to html logic

--- src/commands/website.rs
+++ src/commands/website.rs
@@ -1,19 +1,33 @@
 use super::paths;
 use pulldown_cmark::{html, Options, Parser};
 
+// Removes any previously generated output
 pub fn clean() {
-    // Removes any previously generated output
     paths::remove_dir(&paths::public());
 }
 
+// Generates a static HTML bundle from markdown notes
 pub fn build() {
-    // Generates a static HTML bundle from markdown notes
     paths::create_dir(&paths::public());
-    let markdown_input = "EmptySome **example** testing from trevordmiller.com";
-    let parser = Parser::new_ext(markdown_input, Options::empty());
-    let mut html_output = String::new();
-    html::push_html(&mut html_output, parser);
-    let html_output_in_document = format!(
+    paths::create_file(
+        &paths::public().join("index.html"),
+        &markdown_to_html("Some **markdown** testing from trevordmiller.com"),
+    );
+}
+
+// Adds a CNAME file for the host (GitHub Pages) and registrar (Hover) to use my custom domain name (trevordmiller.com)
+pub fn configure() {
+    paths::create_file(&paths::cname(), "trevordmiller.com");
+}
+
+// Pushes output to my GitHub Pages repo (https://github.com/trevordmiller/trevordmiller.github.io)
+pub fn deploy() {}
+
+fn markdown_to_html(markdown: &str) -> std::string::String {
+    let parser = Parser::new_ext(markdown, Options::empty());
+    let mut html = String::new();
+    html::push_html(&mut html, parser);
+    format!(
         "
         <!DOCTYPE html>
         <html lang=\"en-US\">
@@ -38,19 +52,6 @@ pub fn build() {
               </body>
         </html>
     ",
-        &html_output
-    );
-    paths::create_file(
-        &paths::public().join("index.html"),
-        &html_output_in_document,
-    );
-}
-
-pub fn configure() {
-    // Adds a CNAME file for the host (GitHub Pages) and registrar (Hover) to use my custom domain name (trevordmiller.com)
-    paths::create_file(&paths::cname(), "trevordmiller.com");
-}
-
-pub fn deploy() {
-    // Pushes output to my GitHub Pages repo (https://github.com/trevordmiller/trevordmiller.github.io)
+        &html
+    )
 }
